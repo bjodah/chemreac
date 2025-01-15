1d00c55956e58ee6a355ef3551f63c31cfe01ed9
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-15 23:13:11 +0100

    update install script

--- .ci/ci-01-install.sh
+++ .ci/ci-01-install.sh
@@ -2,12 +2,38 @@
 set -euxo pipefail
 source /opt-3/cpython-v3.*-apt-deb/bin/activate
 INSTALL_PIP_FLAGS="--cache-dir ./ci-cache/pip-cache --upgrade"  # --user
-python3 -m pip install $INSTALL_PIP_FLAGS finitediff "block_diag_ilu>=0.4.3" "pyodeint>=0.10.4" "pygslodeiv2>=0.9.3"  "batemaneq>=0.2.2"
-python3 -c "import block_diag_ilu"
-env \
-    CFLAGS="-isystem $SUNDBASE/include $CFLAGS" \
-    LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS" \
-python3 -m pip install $INSTALL_PIP_FLAGS --upgrade "pycvodes>=0.14.1"
-python3 -m pip install $INSTALL_PIP_FLAGS --upgrade "pyodesys>=0.13.1"
-python3 -m pip install $INSTALL_PIP_FLAGS --upgrade "chempy>=0.7.10"
-ls external/anyode
+for pypkg in pyodeint pygslodeiv2 pycompilation pycodeexport batemaneq finitediff block_diag_ilu pycvodes pyodesys chempy; do
+    case $pypkg in
+         block_diag_ilu)
+             pypkg_fqn="git+https://github.com/bjodah/block_diag_ilu"
+             ;;
+         pycompilation)
+             pypkg_fqn="git+https://github.com/bjodah/pycompilation@use-importlib-rather-than-imp#egg=pycompilation"
+             ;;
+         pycodeexport)
+             pypkg_fqn="git+https://github.com/bjodah/pycodeexport@qulify-extension-name-and-new-ci#egg=pycodeexport"
+             ;;
+         pycvodes)
+             pypkg_fqn="git+https://github.com/bjodah/pycvodes@may21#egg=pycvodes"
+             ;;
+         pyodesys)
+             pypkg_fqn="git+https://github.com/bjodah/pyodesys@bdf2#egg=pycvodes"
+             ;;
+         chempy)
+             pypkg_fqn="git+https://github.com/bjodah/chempy@nov20#egg=chempy"
+             ;;
+         *)
+             pypkg_fqn=$pypkg
+             ;;
+    esac
+    if [[ $pypkg == "pycvodes" ]]; then
+        env \
+            CFLAGS="-isystem $SUNDBASE/include $CFLAGS" \
+            LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS" \
+            py3 -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
+    else
+        py3 -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
+    fi
+    py3 -m pytest -k "not pool_discontinuity_approx" --pyargs $pypkg
+done
+
